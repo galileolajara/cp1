7d05977d5779f48ccd8de9e7d1a9fcc51e7612cc
Galileo Lajara <galileolajara@gmail.com>
2025-01-07 18:28:39 +0800

    renamed GLC to LC

--- token.lc.js
+++ token.lc.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let lines = ["{at(%glc)", "{:token[", "#nil,"];
-let txt = fs.readFileSync("out/glc_parser.h").toString();
+let txt = fs.readFileSync("out/lc_parser.h").toString();
 for (let line of txt.split("\n")) {
    if (line == "") continue;
    lines.push("#" + line.substring(18, line.indexOf(" ", 19)).toLowerCase().split("_").join("-") + ",");
@@ -9,4 +9,4 @@ lines.push("]:int");
 lines.push("{glc-name(e:this):char[] @glc-name}");
 lines.push("}");
 lines.push("}\n");
-fs.writeFileSync("out/token.glc2", lines.join("\n"));
+fs.writeFileSync("out/token.lc", lines.join("\n"));
