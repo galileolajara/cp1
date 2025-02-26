9d089c85f26edeb39d834b9e22167700e1b4d0c0
Galileo Lajara <galileolajara@gmail.com>
2025-02-26 19:31:00 +0800

    used python instead of js for build-*.ninja generation

---
+++ build.ninja.py
@@ -0,0 +1,70 @@
+out = []
+for line in open("build-template.ninja", "r"):
+    if line.startswith(" command = clang "): continue
+    if line.startswith(" command = gcc "): continue
+    out.append(line)
+f = open("build-tcc.ninja", "wb")
+f.write("".join(out).encode("ascii"))
+f.close()
+
+out = []
+for line in open("build-template.ninja", "r"):
+    if line.startswith(" command = tcc "): continue
+    if line.startswith(" command = gcc "): continue
+    out.append(line)
+f = open("build-clang.ninja", "wb")
+f.write("".join(out).encode("ascii"))
+f.close()
+
+out = []
+for line in open("build-template.ninja", "r"):
+    if line.startswith(" command = tcc "): continue
+    if line.startswith(" command = gcc "): continue
+    if line.startswith(" command = clang "):
+        line = "$flags -ggdb".join(line.split("$flags"))
+    out.append(line)
+f = open("build-clang-debug.ninja", "wb")
+f.write("".join(out).encode("ascii"))
+f.close()
+
+out = []
+for line in open("build-template.ninja", "r"):
+    if line.startswith(" command = tcc "): continue
+    if line.startswith(" command = gcc "): continue
+    if line.startswith(" command = clang "):
+        line = "$flags -O3 -flto".join(line.split("$flags"))
+    out.append(line)
+f = open("build-clang-release.ninja", "wb")
+f.write("".join(out).encode("ascii"))
+f.close()
+
+out = []
+for line in open("build-template.ninja", "r"):
+    if line.startswith(" command = tcc "): continue
+    if line.startswith(" command = clang "): continue
+    out.append(line)
+f = open("build-gcc.ninja", "wb")
+f.write("".join(out).encode("ascii"))
+f.close()
+
+out = []
+for line in open("build-template.ninja", "r"):
+    if line.startswith(" command = tcc "): continue
+    if line.startswith(" command = clang "): continue
+    if line.startswith(" command = gcc "):
+        line = "$flags -ggdb".join(line.split("$flags"))
+    out.append(line)
+f = open("build-gcc-debug.ninja", "wb")
+f.write("".join(out).encode("ascii"))
+f.close()
+
+out = []
+for line in open("build-template.ninja", "r"):
+    if line.startswith(" command = tcc "): continue
+    if line.startswith(" command = clang "): continue
+    if line.startswith(" command = gcc "):
+        line = "$flags -O3 -flto".join(line.split("$flags"))
+    out.append(line)
+f = open("build-gcc-release.ninja", "wb")
+f.write("".join(out).encode("ascii"))
+f.close()
