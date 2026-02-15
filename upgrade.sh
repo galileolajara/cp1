edf836d0c28531df932d79f5a605ad658ba4c256
Galileo Lajara <galileolajara@gmail.com>
2026-02-15 09:36:11 +0800

    fixing self-hosting

--- upgrade.sh
+++ upgrade.sh
@@ -7,6 +7,7 @@ cp out/cp1_parse.c cp1_parse.c
 cp out/parse2.cp1.c parse.cp1.c
 cp out/compile2.cp1.c compile.cp1.c
 cp out/run2.cp1.c run.cp1.c
+cp out/meta2.cp1.c meta.cp1.c
 cp out/build-crc32c.h build-crc32c.h
 cp hashtable2.c hashtable.c
 rm -rf cp1-tmp-* out bin/cp1-parse bin/cp1-run bin/cp1-compile bin/cp1-qjs
